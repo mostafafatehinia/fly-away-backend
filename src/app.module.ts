2860c8c9733006cf28a983cc90a0252bce89a1bd
mo3tafa <mostafafatehinia@gmail.com>
2025-08-19 14:45:09 +0330

    feat(config): add database and env configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,44 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
+import appConfig from './configs/app.config';
+import dbConfig from './configs/database.config';
+import { environmentConfigValidation } from './validations/environment.validation';
+
+const ENV = process.env.NODE_ENV;
+
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, dbConfig],
+      validationSchema: environmentConfigValidation,
+      envFilePath: ENV === 'development' ? '.env.local' : `.env.${ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('database.host'),
+          port: configService.get('database.port'),
+          username: configService.get('database.username'),
+          password: configService.get('database.password'),
+          database: configService.get('database.database'),
+          autoLoadEntities: true,
+          synchronize: ENV === 'development',
+        };
+      },
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
