dff427f3827da9ee726f09cd66af804e4151d91d
mo3tafa <mostafafatehinia@gmail.com>
2025-08-20 09:25:13 +0330

    feat(users): add user model

---
+++ src/enums/user.enum.ts
@@ -0,0 +1,12 @@
+export enum UserStatus {
+  ACTIVE = 'active',
+  INACTIVE = 'inactive',
+  BANNED = 'banned',
+}
+
+export enum UserRole {
+  USER = 'user',
+  ADMIN = 'admin',
+  MANAGER = 'manager',
+  SUPER_ADMIN = 'super_admin',
+}
